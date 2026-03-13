8c1d03e92dcddcb37d5f762801e41fb655f72351
lwx <2213196412@qq.com>
2026-03-13 16:57:40 +0800

    更新point

--- point.go
+++ point.go
@@ -1,38 +1,25 @@
 package core
 
-// Row 包含一个数据点以及用于标识一种指标的属性。
-type Row struct {
-	// 指标的唯一名称。
-	// 必须设置此字段。
-	Metric string
-	// 用于进一步详细标识的可选键值属性。
-	Labels []Label
-	// 必须设置此字段。
-	DataPoint
-}
-
-// DataPoint 表示一个数据点，是时序数据的最小单位。
-type DataPoint struct {
-	// 实际值。必须设置此字段。
-	Value float64
-	// Unix 时间戳。
-	Timestamp int64
-}
-
 const (
-	// 标签名称的最大长度。
-	//
-	// 更长的名称会被截断。
-	maxLabelNameLen = 256
-
-	// 标签值的最大长度。
-	//
-	// 更长的值会被截断。
+	maxLabelNameLen  = 256
 	maxLabelValueLen = 16 * 1024
 )
 
-// Label 是一个时序标签。
-// 缺少名称或值的标签是无效的。
+// Row 包含一个数据点以及用于标识一种指标的属性
+type Row struct {
+	Metric string  // 指标的唯一名称，必须设置此字段
+	Labels []Label // 用于进一步详细标识的可选键值属性
+	DataPoint
+}
+
+// DataPoint 表示一个数据点，是时序数据的最小单位
+type DataPoint struct {
+	Value     float64
+	Timestamp int64
+}
+
+// Label 是一个时序标签
+// 缺少名称或值的标签是无效的
 type Label struct {
 	Name  string
 	Value string
